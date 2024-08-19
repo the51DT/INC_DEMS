56c58ac41c4f9a5b20f50cd417326fb3a70bcb10
100minkyung <mkbaek@the-51.com>
2024-08-19 17:54:58 +0900

    시승 행사 관리 / 시승 동의서 관리 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", () => {
   const headerBtn = document.querySelector(".header_btn")
-  headerBtn.addEventListener("click", function() {
-    const lnb = document.querySelector(".lnb_admin")
-    lnb.classList.toggle("on")
-  })
+  const headerFn = () => {
+    headerBtn.addEventListener("click", () => {
+      const lnb = document.querySelector(".lnb_admin");
+      lnb.classList.toggle("on");
+    });
+  }
+  headerBtn ? headerFn() : false;
+
+  const pop = document.querySelectorAll(".popup_wrap")
+  const popCloseBtnFn = (btn) => {
+    btn.forEach((el) => {
+      el.addEventListener("click", () => {
+        el.closest(".popup_wrap").classList.remove("on");
+      });
+    })
+  }
+  const popupOpenBtnFn = (btn) => {
+    btn.forEach((el) => {
+      el.addEventListener("click", () => {
+        document.querySelector(`#${el.dataset.pop}`).classList.add("on");
+      });
+    });
+  }
+  const popFn = function(){
+    popCloseBtnFn(document.querySelectorAll(".popClose"));
+    popupOpenBtnFn(document.querySelectorAll(".popOpen"));
+  }
+  pop ? popFn() : false;
 })
